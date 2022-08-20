ee3aa64cdd98a43bd473a670dcd13825de0fc439
Duhan Kosali <71566458+duhankosali@users.noreply.github.com>
2022-08-20 20:10:13 +0300

    The Bank App
    The libraries required for the project have been downloaded.
    
    SqlServer connection established using Entity Framework

---
+++ Dk.BankApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Dk.BankApp.Web.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dk.BankApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BankContext _context; // Dependency Injection
+        public HomeController(BankContext context) // constructor
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
